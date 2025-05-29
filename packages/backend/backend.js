3b0aaf4b6a70789aa34d6caff1083777fd6de38f
juanito-lenteja <juan@fmalpartida.com>
2025-05-29 13:59:24 -0700

    fix: fixed package.json to reflect actual build name

--- packages/backend/backend.js
+++ packages/backend/backend.js
@@ -15,6 +15,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(cors());
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -39,8 +42,6 @@ const corsOptions = {
   credentials: false,
 };*/
 //app.use(cors(corsOptions));
-app.use(express.json());
-app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
