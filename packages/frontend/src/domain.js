7c28231f00496c8714ede618fc1d0bac58eca07e
EllaHanson <ellahanson.126@gmail.com>
2025-06-04 23:31:48 -0700

    Change Domain Back

--- packages/frontend/src/domain.js
+++ packages/frontend/src/domain.js
@@ -1,3 +1,3 @@
-//const domain = "https://woofer-cvcqfce3ada9cgb2.westus3-01.azurewebsites.net";
-const domain = "http://localhost:8000";
+const domain = "https://woofer-cvcqfce3ada9cgb2.westus3-01.azurewebsites.net";
+//const domain = "http://localhost:8000";
 export default domain;
