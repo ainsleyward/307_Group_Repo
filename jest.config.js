f7291c18f754ca8053eae31c4fa6263b5da5f5b4
juanito-lenteja <juan@fmalpartida.com>
2025-06-06 22:58:26 -0700

    fix: lint

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
 // jest.config.js
 module.exports = {
-    testEnvironment: "jsdom",
-    testMatch: ["**/frontend_tests/**/*.[jt]s?(x)"],
-    transform: {
-      "^.+\\.[jt]sx?$": "babel-jest",
-    },
-    moduleNameMapper: {
-      "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    },
-    setupFiles: ["<rootDir>/jest.setup.js"],
-  };
-  
+  testEnvironment: "jsdom",
+  testMatch: ["**/frontend_tests/**/*.[jt]s?(x)"],
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFiles: ["<rootDir>/jest.setup.js"],
+};
